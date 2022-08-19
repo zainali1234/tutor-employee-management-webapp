b384810779608d29bf1821f3519dc24c91a4f94b
Zain Ali <67614246+zainali1234@users.noreply.github.com>
2022-08-19 14:03:00 -0500

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,66 +1,29 @@
+# handles all user admin functionalities
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_user, login_required, current_user, logout_user
 from .models import SessionForm, Employee, Student
 from . import db
 admin = Blueprint('admin', __name__)
 
-@admin.route('/account/employee-database')
-@login_required
-def employee_database():
-    return render_template('admin-employee-database.html', user=current_user, employees=Employee.query.all(), sessions=SessionForm.query.all())
-
-@admin.route('/account/session-database')
-@login_required
-def session_database():
-    return render_template('admin-session-database.html', user=current_user, employees=Employee.query.all(), sessions=SessionForm.query.all())
-
+# renders webpage that allows user to choose what student database action page to be redirected to 
 @admin.route('/account/manage-student-database')
 @login_required
 def manage_student_database():
     return render_template('admin-manage-student-database.html', user=current_user, employees=Employee.query.all(), sessions=SessionForm.query.all(),
                            students=Student.query.all())
 
-@admin.route('/account/manage-employee-database')
-@login_required
-def manage_employee_database():
-    return render_template('admin-manage-employee-database.html', user=current_user, employees=Employee.query.all(), sessions=SessionForm.query.all(),
-                           students=Student.query.all())
-
-@admin.route('/account/delete-employee', methods=['GET','POST'])
-@login_required
-def delete_employee_database():
-    if request.method == 'POST':
-        employee_id = request.form.get("delete-employee-id")
-        Employee.query.filter(Employee.id == employee_id).delete()
-        db.session.commit()
-    return render_template('admin-delete-employee.html', user=current_user, employees=Employee.query.all(), sessions=SessionForm.query.all(),
-                           students=Student.query.all())
-
-@admin.route('/account/manage-session-database')
-@login_required
-def manage_session_database():
-    return render_template('admin-manage-session-database.html', user=current_user, employees=Employee.query.all(), sessions=SessionForm.query.all(),
-                           students=Student.query.all())
-
-@admin.route('/account/delete-session', methods=['GET','POST'])
-@login_required
-def delete_session_database():
-    if request.method == 'POST':
-        session_id = request.form.get("delete-session-id")
-        SessionForm.query.filter(SessionForm.id == session_id).delete()
-        db.session.commit()
-    return render_template('admin-delete-session.html', user=current_user, employees=Employee.query.all(), sessions=SessionForm.query.all(),
-                           students=Student.query.all())
-
+# renders all students registered within the database
 @admin.route('/account/student-database', methods=['GET','POST'])
 @login_required
 def student_database():
     return render_template('admin-student-database.html', user=current_user, employees=Employee.query.all(),
                            sessions=SessionForm.query.all(), students=Student.query.all())
 
+# prompts user to enter information about a student to be entered into the database
 @admin.route('/account/register-student', methods=['GET','POST'])
 @login_required
 def register_student():
+    # if user submits student information, then check conditions to make sure requested account info is valid
     if request.method == 'POST':
         student_name = request.form.get("student-name")
         grade_level = request.form.get("grade-level")
@@ -82,6 +45,7 @@ def register_student():
         elif len(parent_email) < 2:
             flash('Email must be greater than 2 characters.', category='error')
         else:
+            # if all conditions are met, new_student object is created with input attributes and is added to the database 
             new_student = Student(name=student_name, grade_level=grade_level,
                                   age=student_age, parent_name=parent_name, parent_email=parent_email,
                                   active_status=True, other_information=other_info)
@@ -91,6 +55,7 @@ def register_student():
     return render_template('admin-register-student.html', user=current_user, employees=Employee.query.all(),
                            sessions=SessionForm.query.all(), students=Student.query.all())
 
+# prompts user to choose a student to delete from the database
 @admin.route('/account/delete-student', methods=['GET','POST'])
 @login_required
 def delete_student_database():
@@ -101,4 +66,52 @@ def delete_student_database():
     return render_template('admin-delete-student.html', user=current_user, employees=Employee.query.all(), sessions=SessionForm.query.all(),
                            students=Student.query.all())
 
+
+# renders webpage that allows user to choose what session database action page to be redirected to 
+@admin.route('/account/manage-session-database')
+@login_required
+def manage_session_database():
+    return render_template('admin-manage-session-database.html', user=current_user, employees=Employee.query.all(), sessions=SessionForm.query.all(),
+                           students=Student.query.all())
+
+# renders all sessions registered within the database
+@admin.route('/account/session-database')
+@login_required
+def session_database():
+    return render_template('admin-session-database.html', user=current_user, employees=Employee.query.all(), sessions=SessionForm.query.all())
+
+# prompts user to choose a session to delete from the database
+@admin.route('/account/delete-session', methods=['GET','POST'])
+@login_required
+def delete_session_database():
+    if request.method == 'POST':
+        session_id = request.form.get("delete-session-id")
+        SessionForm.query.filter(SessionForm.id == session_id).delete()
+        db.session.commit()
+    return render_template('admin-delete-session.html', user=current_user, employees=Employee.query.all(), sessions=SessionForm.query.all(),
+                           students=Student.query.all())
+
+# renders webpage that allows user to choose what employee database action page to be redirected to 
+@admin.route('/account/manage-employee-database')
+@login_required
+def manage_employee_database():
+    return render_template('admin-manage-employee-database.html', user=current_user, employees=Employee.query.all(), sessions=SessionForm.query.all(),
+                           students=Student.query.all())
+
+# renders all employees registered within the database
+@admin.route('/account/employee-database')
+@login_required
+def employee_database():
+    return render_template('admin-employee-database.html', user=current_user, employees=Employee.query.all(), sessions=SessionForm.query.all())
+
+# prompts user to choose an employee to delete from the database
+@admin.route('/account/delete-employee', methods=['GET','POST'])
+@login_required
+def delete_employee_database():
+    if request.method == 'POST':
+        employee_id = request.form.get("delete-employee-id")
+        Employee.query.filter(Employee.id == employee_id).delete()
+        db.session.commit()
+    return render_template('admin-delete-employee.html', user=current_user, employees=Employee.query.all(), sessions=SessionForm.query.all(),
+                           students=Student.query.all())
 
